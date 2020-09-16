9d90035b8231323637bc5882bcbc109033020a82
qurek9038 <qurek9038@wrdsb.ca>
2020-09-16 06:17:33 +0000

    Input-Output Question 2 (Not Done)

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program asks the user for two numbers and then calculates the quotient of that number and the remainder
+ * @author Khazina
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Ask the user for two integers 
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+
+    //Get the users first integer
+    int firstInteger = input.nextInt();
+
+    //Get the users second integer
+    int secondInteger = input.nextInt();
+
+    //
+    int pink = firstInteger % secondInteger;
+
     
   }
 }
